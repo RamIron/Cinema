eb434ecceb368718b9ba537b64a1c57ecd96accd
Mathias Zunino <mathiaszunino@gmail.com>
2019-06-12 00:37:10 -0300

    mapOfFunciones added in Sala

--- Clases/Sala.hh
+++ Clases/Sala.hh
@@ -1,7 +1,9 @@
 #ifndef PALAB06_SALA_HH
 #define PALAB06_SALA_HH
 
+#include "Funcion.hh"
 #include <iostream>
+#include <map>
 
 using namespace std;
 
@@ -11,6 +13,7 @@ private:
   int id;
   static int currentId;
   int capacidad;
+  map<int, Funcion *> mapOfFunciones;
 
 public:
   Sala();
