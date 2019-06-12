099121474cc76e3f93903f88313c427c1937eddb
Mathias Zunino <mathiaszunino@gmail.com>
2019-06-12 00:45:21 -0300

    fixing includes in Funcion

--- Clases/Funcion.cpp
+++ Clases/Funcion.cpp
@@ -2,31 +2,36 @@
 
 int Funcion::currentId = 0;
 
-//Constructores
+// Constructores
 Funcion::Funcion() { this->id = currentId++; }
 
-Funcion::Funcion(DtFecha dia, DtHorario horario, float precio) {
-	this->id = currentId;
-	this->dia = dia;
-	this->horario = horario;
-	this->precio;
+Funcion::Funcion(DtFecha fecha, DtHorario horario, float precio) {
+  this->id = currentId;
+  this->fecha = fecha;
+  this->horario = horario;
+  this->precio;
 }
 
-Funcion::Funcion(DtFecha dia, DtHorario horario){
-	this->id = currentId;
-	this->dia = dia;
-	this->horario = horario;
+Funcion::Funcion(DtFecha fecha, DtHorario horario) {
+  this->id = currentId;
+  this->fecha = fecha;
+  this->horario = horario;
 }
 
-//Getters y Setters
-int Funcion::getID() {return this->id; }
-DtFecha Funcion::getFecha() {return this->dia; }
-DtHorario Funcion::getHorario() {return this->horario; }
-float Funcion::getPrecio() {return this->precio; }
+// Getters y Setters
+int Funcion::getID() { return this->id; }
 
-void Funcion::setFecha(DtFecha dia) {this->dia = dia; }
-void Funcion::setHorario(DtHorario horario) {this->horario = horario; }
-void Funcion::setPrecio(float precio) {this->precio = precio; }
+DtFecha Funcion::getFecha() { return this->fecha; }
 
-//Destructor
+DtHorario Funcion::getHorario() { return this->horario; }
+
+float Funcion::getPrecio() { return this->precio; }
+
+void Funcion::setFecha(DtFecha fecha) { this->fecha = fecha; }
+
+void Funcion::setHorario(DtHorario horario) { this->horario = horario; }
+
+void Funcion::setPrecio(float precio) { this->precio = precio; }
+
+// Destructor
 Funcion::~Funcion() {}
--- Clases/Funcion.hh
+++ Clases/Funcion.hh
@@ -1,41 +1,48 @@
 #ifndef PALAB06_FUNCION_HH
 #define PALAB06_FUNCION_HH
 
-#include <iostream>
-#include "DtFecha.hh"
-#include "DtHorario.hh"
+#include "Datatypes/DtFecha.hh"
+#include "Datatypes/DtHorario.hh"
 #include "Reserva.hh"
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-class Funcion{
-	private:
-		int id;
-		static int currentId;
-		DtFecha dia;
-		DtHorario horario;
-		float precio;
-		vector<Reserva *> reservas;
-		
-	public:
-		//Constructores
-		Funcion();
-		Funcion(DtFecha dia, DtHorario horario, float precio);
-		Funcion(DtFecha dia, DtHorario horario);
-				
-		//Getters y Setters
-		int getID();
-		DtFecha getFecha();
-		DtHorario getHorario();
-		float getPrecio();
-		
-		void setFecha(DtFecha dia);
-		void setHorario(DtHorario horario);
-		void setPrecio(float precio);
-		
-		//Destructor
-		~Funcion();
+class Funcion {
+private:
+  int id;
+  static int currentId;
+  DtFecha fecha;
+  DtHorario horario;
+  float precio;
+  vector<Reserva *> reservas;
+
+public:
+  // Constructores
+  Funcion();
+
+  Funcion(DtFecha fecha, DtHorario horario, float precio);
+
+  Funcion(DtFecha fecha, DtHorario horario);
+
+  // Getters y Setters
+  int getID();
+
+  DtFecha getFecha();
+
+  DtHorario getHorario();
+
+  float getPrecio();
+
+  void setFecha(DtFecha fecha);
+
+  void setHorario(DtHorario horario);
+
+  void setPrecio(float precio);
+
+  // Destructor
+  ~Funcion();
 };
 
 #endif
