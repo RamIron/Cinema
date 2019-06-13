d9e5dabac44bee6481b3797714bcbccaee9fbdfa
totozunino <tecnoinf@tecnoinf203.esi.edu.uy>
2019-06-13 18:20:25 -0300

    remove id from class Puntaje

--- Clases/Puntaje.cpp
+++ Clases/Puntaje.cpp
@@ -1,18 +1,13 @@
 #include "Puntaje.hh"
 
-int Puntaje::currentId = 0;
-
 // Constructores
-Puntaje::Puntaje() { this->id = currentId++; }
+Puntaje::Puntaje() {}
 
 Puntaje::Puntaje(int puntos) {
-  this->id = currentId++;
   this->puntos = puntos;
 }
 
 // Getters y Setters
-int Puntaje::getId() { return this->id; }
-
 int Puntaje::getPuntos() { return this->puntos; }
 
 void Puntaje::setPuntos(int puntos) { this->puntos = puntos; }
