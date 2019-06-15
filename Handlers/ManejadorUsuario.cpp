bf07f32d54331ad3896cf4f1668bb32b0c15eb20
Mathias Zunino <mathiaszunino@gmail.com>
2019-06-15 16:27:20 -0300

    ManejadorUsuario preparado y listo para la accion

---
+++ Handlers/ManejadorUsuario.cpp
@@ -0,0 +1,39 @@
+#include "ManejadorUsuario.hh"
+
+ManejadorUsuario *ManejadorUsuario::muInstance = NULL;
+
+ManejadorUsuario::ManejadorUsuario() {}
+
+ManejadorUsuario *ManejadorUsuario::getInstance() {
+  if (muInstance == NULL) {
+    muInstance = new ManejadorUsuario();
+  }
+  return muInstance;
+}
+
+void ManejadorUsuario::agregarUsuario(Usuario *usuario) {
+  this->usuarios.insert(make_pair(usuario->getNickname(), usuario));
+}
+
+bool ManejadorUsuario::existeUsuario(string nickname) {
+  map<string, Usuario *>::iterator it;
+  it = this->usuarios.find(nickname);
+  return it == this->usuarios.end();
+}
+
+Usuario *ManejadorUsuario::obtenerUsuario(string nickname) {
+  map<string, Usuario *>::iterator it;
+  it = this->usuarios.find(nickname);
+  return it->second;
+}
+
+void ManejadorUsuario::eliminarUsuario(string nickname) {
+  map<string, Usuario *>::iterator it;
+  it = this->usuarios.find(nickname);
+  delete it->second;
+  this->usuarios.erase(nickname);
+}
+
+map<string, Usuario *> ManejadorUsuario::obtenerUsuarios() {
+  return this->usuarios;
+}
--- Handlers/ManejadorUsuario.hh
+++ Handlers/ManejadorUsuario.hh
@@ -1,24 +1,32 @@
 #ifndef PALAB06_MANEJADORUSUARIO_HH
 #define PALAB06_MANEJADORUSUARIO_HH
-#include "./Clases/Usuario.hh"
+
+#include "../Clases/Usuario.hh"
 #include <map>
-#include <vector>
 
 using namespace std;
 
 class ManejadorUsuario {
 private:
-  static ManejadorUsuario *instancia;
-  map<string, Usuario *> usuarios;
-  ManejadorUsuario();
+    static ManejadorUsuario *muInstance;
+    map<string, Usuario *> usuarios;
+
+    ManejadorUsuario();
 
 public:
-  static ManejadorUsuario *getInstancia();
-  vector<Usuario *> obtenerUsuarios();
-  Usuario *obtenerUsuario(string nickname);
-  void agregarUsuario(Usuario *);
-  bool existeUsuario(string nickname);
-  void eliminarUsuario(string nickname);
-  virtual ~ManejadorUsuario();
+    static ManejadorUsuario *getInstance();
+
+    map<string, Usuario *> obtenerUsuarios();
+
+    Usuario *obtenerUsuario(string nickname);
+
+    void agregarUsuario(Usuario *);
+
+    bool existeUsuario(string nickname);
+
+    void eliminarUsuario(string nickname);
+
+    virtual ~ManejadorUsuario();
 };
+
 #endif
