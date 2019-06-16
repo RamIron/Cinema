7497d43a3ef7b1cbdbd423315a8a236652537e8b
Mathias Zunino <mathiaszunino@gmail.com>
2019-06-16 18:53:38 -0300

    ManejadorFinanciera created

---
+++ Handlers/ManejadorFinanciera.cpp
@@ -0,0 +1,41 @@
+#include "ManejadorFinanciera.hh"
+
+ManejadorFinanciera *ManejadorFinanciera::mfInstance = NULL;
+
+ManejadorFinanciera::ManejadorFinanciera(){}
+
+ManejadorFinanciera *ManejadorFinanciera::getInstance() {
+  if (mfInstance == NULL) {
+    mfInstance = new ManejadorFinanciera();
+  }
+  return mfInstance;
+}
+
+void ManejadorFinanciera::agregarFinanciera(Financiera * financiera){
+  this->financieras.insert(make_pair(financiera->getNombre(), financiera));
+}
+
+bool ManejadorFinanciera::existeFinanciera(string financiera){
+  map<string, Financiera *>::iterator it;
+  it = this->financieras.find(financiera);
+  return it == this->financieras.end();
+}
+
+Financiera *ManejadorFinanciera::obtenerFinanciera(string financiera){
+  map<string, Financiera *>::iterator it;
+  it = this->financieras.find(financiera);
+  return it->second;
+}
+
+void ManejadorFinanciera::eliminarFinanciera(string financiera){
+  map<string, Financiera *>::iterator it;
+  it = this->financieras.find(financiera);
+  delete it->second;
+  this->financieras.erase(financiera);
+}
+
+map<string, Financiera *> ManejadorFinanciera::obtenerFinancieras(){
+  return this->financieras;
+}
+
+ManejadorFinanciera::~ManejadorFinanciera(){}
---
+++ Handlers/ManejadorFinanciera.hh
@@ -0,0 +1,32 @@
+#ifndef PALAB06_MANEJADORFINANCIERA_HH
+#define PALAB06_MANEJADORFINANCIERA_HH
+
+#include "../Clases/Financiera.hh"
+#include <map>
+
+using namespace std;
+
+class ManejadorFinanciera {
+private:
+    static ManejadorFinanciera *mfInstance;
+    map<string, Financiera *> financieras;
+
+    ManejadorFinanciera();
+
+public:
+    static ManejadorFinanciera *getInstance();
+
+    map<string, Financiera *> obtenerFinancieras();
+
+    Financiera *obtenerFinanciera(string financiera);
+
+    void agregarFinanciera(Financiera *);
+
+    bool existeFinanciera(string financiera);
+
+    void eliminarFinanciera(string fiananciera);
+
+    ~ManejadorFinanciera();
+};
+
+#endif
