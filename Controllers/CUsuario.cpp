3d60d730f85edc435c26d8e360024885722b3982
Mathias Zunino <mathiaszunino@gmail.com>
2019-06-17 17:27:10 -0300

    Iniciar Sesion fixed

--- Controllers/CUsuario.cpp
+++ Controllers/CUsuario.cpp
@@ -1,4 +1,6 @@
 #include "CUsuario.hh"
+#include "../Clases/Sesion.hh"
+#include "ManejadorUsuario.hh"
 
 CUsuario *CUsuario::cuInstance = NULL;
 
@@ -14,11 +16,15 @@ CUsuario *CUsuario::getInstance() {
 void CUsuario::ingresaNick(string nickname) { this->nickname = nickname; }
 
 bool CUsuario::ingresaPass(string contrasenia) {
-    ManejadorUsuario *mu = ManejadorUsuario::getInstance();
-    if(mu.existeUsuario(this->nickname)){
-        Usuario *u = mu.obtenerUsuario(this->nickname);
-        return strcmp(u.getConstrasenia(), contrasenia) == 0;
+  ManejadorUsuario *mu = ManejadorUsuario::getInstance();
+  if (mu->existeUsuario(this->nickname)) {
+    Usuario *u = mu->obtenerUsuario(this->nickname);
+    if (u->getConstrasenia().compare(contrasenia) == 0) {
+      auto sesion = Sesion::getInstance();
+      sesion->setUsuario(u);
     }
+    return u->getConstrasenia().compare(contrasenia) == 0;
+  }
 }
 
 CUsuario::~CUsuario() {}
