6702869527cddb359554d53083ddf203c29dd1b7
Mathias Zunino <mathiaszunino@gmail.com>
2019-06-20 15:26:38 -0300

    manejadorComentario created

---
+++ Handlers/ManejadorComentario.cpp
@@ -0,0 +1,41 @@
+#include "ManejadorComentario.hh"
+
+ManejadorComentario *ManejadorComentario::mcInstance = NULL;
+
+ManejadorComentario::ManejadorComentario(){}
+
+ManejadorComentario *ManejadorComentario::getInstance() {
+  if (mcInstance == NULL) {
+    mcInstance = new ManejadorComentario();
+  }
+  return mcInstance;
+}
+
+void ManejadorComentario::agregarComentario(Comentario * comentario){
+  this->comentarios.insert(make_pair(comentario->getId(), comentario));
+}
+
+bool ManejadorComentario::existeComentario(int id){
+  map<int, Comentario *>::iterator it;
+  it = this->comentarios.find(id);
+  return it != this->comentarios.end();
+}
+
+Comentario *ManejadorComentario::obtenerComentario(int id){
+  map<int, Comentario *>::iterator it;
+  it = this->comentarios.find(id);
+  return it->second;
+}
+
+void ManejadorComentario::eliminarComentario(int id){
+  map<int, Comentario *>::iterator it;
+  it = this->comentarios.find(id);
+  delete it->second;
+  this->comentarios.erase(id);
+}
+
+map<int, Comentario *> ManejadorComentario::obtenerComentarios(){
+  return this->comentarios;
+}
+
+ManejadorComentario::~ManejadorComentario(){}
---
+++ Handlers/ManejadorComentario.hh
@@ -0,0 +1,32 @@
+#ifndef PALAB06_MANEJADORCOMENTARIO_HH
+#define PALAB06_MANEJADORCOMENTARIO_HH
+
+#include "../Clases/Comentario.hh"
+#include <map>
+
+using namespace std;
+
+class ManejadorComentario {
+private:
+    static ManejadorComentario *mcInstance;
+    map<int, Comentario *> comentarios;
+
+    ManejadorComentario();
+
+public:
+    static ManejadorComentario *getInstance();
+
+    map<int, Comentario *> obtenerComentarios();
+
+    Comentario *obtenerComentario(int id);
+
+    void agregarComentario(Comentario *);
+
+    bool existeComentario(int id);
+
+    void eliminarComentario(int id);
+
+    ~ManejadorComentario();
+};
+
+#endif
